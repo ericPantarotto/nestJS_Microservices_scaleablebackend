af20970d708ae41112b7dee833104e19fdee0b9e
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-06-16 23:25:56 +0200

    3. Passport

--- sleepr/apps/auth/src/auth.module.ts
+++ sleepr/apps/auth/src/auth.module.ts
@@ -1,11 +1,35 @@
+import { LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import * as Joi from 'joi';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
-import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [UsersModule, LoggerModule],
+  imports: [
+    UsersModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.number().default(3600), // Default to 1 hour
+        PORT: Joi.number().default(3001),
+      }),
+    }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get<string>('JWT_EXPIRATION')}s`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
